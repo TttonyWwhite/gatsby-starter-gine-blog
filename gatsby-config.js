fd6859816697fd781ac72e253af976253e26421d
illiant <effortblesses@outlook.com>
2020-04-21 16:20:49 +0800

    Update gatsby-config.js
    update

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,13 +3,13 @@ module.exports = {
         {
             resolve: `gatsby-theme-gine-blog`,
             options: {
-                configTable: "https://www.notion.so/b4af861710064848aaf9f859c79bb127?v=0de244d6f8414ade9b7b147adcb2f78e"
+                configTable: "https://www.notion.so/468effd7f3f54454a7bf0b8464a95437?v=f39474ad6118488ea07445599fb0ca30"
             }
         },
         {
             resolve: `gatsby-source-notion-database`,
             options: {
-                configTable: "https://www.notion.so/65b35da1762e4f259904bb4cc38e54fd?v=89e80623e52e4419a35f20e54056cfb8"
+                configTable: "https://www.notion.so/b4cd49c3bfe3433fbb3203ab78cc5057?v=ae958766cf244f7bbf32c037a2c10252"
             }
         }
     ],
